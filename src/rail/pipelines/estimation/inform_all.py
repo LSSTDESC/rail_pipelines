4ec602d373cf1d81549ee15c5ec5263b8821c155
Alex Malz <aimalz@nyu.edu>
2023-07-03 10:42:14 -0400

    rename SimpleSOM

--- src/rail/pipelines/estimation/inform_all.py
+++ src/rail/pipelines/estimation/inform_all.py
@@ -47,7 +47,7 @@ class InformPipeline(RailPipeline):
             hdf5_groupname='',
         )
         
-        self.inform_simplesom = Inform_SimpleSOMSummarizer.build(
+        self.inform_simplesom = MiniSOMInformer.build(
             model=os.path.join(namer.get_data_dir(DataType.model, ModelType.estimator), "model_simplesom.pkl"),
              hdf5_groupname='',
         )
