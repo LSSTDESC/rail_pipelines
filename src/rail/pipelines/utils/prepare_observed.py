172ab0ce4e5784adfdb9602e5d652d79121740d4
Eric Charles <badass@stanford.edu>
2025-06-12 16:24:52 -0700

    Fix prepare_observed.py

--- src/rail/pipelines/utils/prepare_observed.py
+++ src/rail/pipelines/utils/prepare_observed.py
@@ -33,23 +33,27 @@ class PrepareObservedPipeline(RailPipeline):
         DS.__class__.allow_overwrite = True
 
         active_catalog_config = catalog_utils.CatalogConfigBase.active_class()
-        band_name_dict = active_catalog_config.band_name_dict()
 
         self.flux_to_mag = LSSTFluxToMagConverter.build(
-            flux_name="{band}_cModelFlux",
-            flux_err_name="{band}_cModelFluxErr",
-            mag_name=active_catalog_config.band_template,
-            mag_err_name=active_catalog_config.band_err_template,
+            flux_name="{band}Flux",
+            flux_err_name="{band}FluxErr",
+            mag_name="{band}Mag",
+            mag_err_name="{band}MagErr",
+            bands=active_catalog_config.bandlist,
             copy_cols=dict(
                 objectId='objectId',
-                ra='coord_ra',
-                dec='coord_dec',
+                coord_ra='coord_ra',
+                coord_dec='coord_dec',
             )
         )
 
         self.deredden = Dereddener.build(
             connections = dict(input=self.flux_to_mag.io.output),
             dustmap_dir=dustmap_dir,
-            mag_name=active_catalog_config.band_template,
+            dustmap_name='sfd',
+            ra_name='coord_ra',
+            dec_name='coord_dec',
+            # band_a_env=active_catalog_config.a_env_dict,
+            mag_name="{band}Mag",
             copy_all_cols=True,
         )
