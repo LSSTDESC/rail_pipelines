86f530d09572bc9f20d71dd48f07d52ec0d511e4
Tianqing Zhang <ztq1996@live.com>
2026-03-18 20:20:58 -0400

    fix test

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -14,7 +14,7 @@ import pytest
         'rail.pipelines.estimation.tomography.TomographyPipeline',
         'rail.pipelines.utils.prepare_observed.PrepareObservedPipeline',
         'rail.pipelines.examples.goldenspike.goldenspike.GoldenspikePipeline',
-        'rail.pipelines.examples.survey_nonuniformity.survey_nonuniformity.SurveyNonuniformDegraderPipeline'
+        'rail.pipelines.examples.survey_nonuniformity.survey_nonuniformity.SurveyNonuniformDegraderPipeline',
     ]
 )
 def test_build_and_read_pipeline(pipeline_class):
